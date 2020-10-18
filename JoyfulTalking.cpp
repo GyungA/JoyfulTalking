253ecc747c66e3fa28fcb4c6943f06fe37f89630
Moon9898 <gga612@naver.com>
2020-10-18 23:47:02 +0900

    drop, pick으로 정답 여부 확인

--- JoyfulTalking.cpp
+++ JoyfulTalking.cpp
@@ -8,6 +8,7 @@ int main()
 
 	ScenePtr scene = Scene::create("방", "Images/방.png");
 
+	// 시작 및 종료 버튼, 아기, 말풍선과 지시어 등 기본 환경 설정
 	auto ObjectX = 700, ObjectY = 440;
 	ObjectPtr bubble = Object::create("Images/말풍선.png", scene, ObjectX-100, ObjectY-140);
 	bubble->setScale(0.4f);
@@ -27,26 +28,95 @@ int main()
 	auto startButton = Object::create("Images/start.png", scene, 590, 70);
 	auto endButton = Object::create("Images/end.png", scene, 590, 20);
 
-	// 지시어가 뜬다
-	showMessage("'치킨'이라고 말해보자! *알맞은 그림이 뜰 때 아무 키나 누르세요");
-
-	auto timer1 = Timer::create(0.8f);
+	//게임 시작 버튼을 누르면 지시어가 뜨고 타이머가 실행된다.
+	auto timer1 = Timer::create(0.4f);
+	auto timer2 = Timer::create(0.4f);
+	auto timer3 = Timer::create(0.4f);
+	auto timer4 = Timer::create(0.4f);
+	auto timer5 = Timer::create(0.4f);
 	showTimer(timer1);
-	timer1->setOnTimerCallback([&](TimerPtr timer) -> bool {
-		chicken->hide();
-		dog->show();
-		return true;
-		});
+	showTimer(timer2);
 
 	startButton->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
 		startButton->hide();
 		endButton->hide();
+
+		showMessage("'치킨'이라고 말해보자! *알맞은 그림이 뜰 때 아무 키나 누르세요");
+		timer1->start();
+
+		return true;
+		});
+
+	timer1->setOnTimerCallback([&](TimerPtr timer) -> bool {
+		chicken->hide();
+		dog->show();
+
+		chicken->drop();
+		dog->pick();
+
+		timer2->start();
+
+		return true;
+		});
+
+	timer2->setOnTimerCallback([&](TimerPtr timer) -> bool {
+		dog->hide();
+		rice->show();
+
+		dog->drop();
+		rice->pick();
+
+		timer3->start();
+
+		return true;
+		});
+
+	timer3->setOnTimerCallback([&](TimerPtr timer) -> bool {
+		rice->hide();
+		love->show();
+
+		rice->drop();
+		love->pick();
+
+		timer4->start();
+
+		return true;
+		});
+
+	timer4->setOnTimerCallback([&](TimerPtr timer) -> bool {
+		love->hide();
+		spoon->show();
+
+		love->drop();
+		spoon->pick();
+
+		timer5->start();
+
+		return true;
+		});
+
+	timer5->setOnTimerCallback([&](TimerPtr timer) -> bool {
+		spoon->hide();
+		chicken->show();
+
+		spoon->drop();
+		chicken->pick();
 
 		timer1->start();
 
 		return true;
 		});
 
+	baby->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
+		if (chicken->isHanded()) {
+			showMessage("성공!");
+		}
+		else {
+			showMessage("실패!");
+		}
+
+		return true;
+		});
 
 	showMessage("'강아지'라고 말해보자!");
 
